02e85ef8543118c4e06cf30bbe7e377e08e9c370
bigdrivernet <you@example.com>
2023-01-10 18:36:31 +0100

    Vörgänger-Nachfolger-Ausgabe

---
+++ Vorgänger-Nachfolger-Ausgabe/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Vorgänger_Nachfolger_Ausgabe
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            ushort usPrevious, usInput, usNext;
+            Console.Write("Bitte geben Sie eine Zahl zwischen 0 und 65535 ein: ");
+            usInput = ushort.Parse(Console.ReadLine());
+
+            usPrevious = (ushort)(usInput - 1);
+            usNext = (ushort)(usInput + 1);
+
+            Console.WriteLine($"Der Vorgänger ist: {usPrevious}");
+            Console.WriteLine($"Der Nachfolger ist: {usNext}");
+        }
+    }
+}
