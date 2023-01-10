9363f31e7d729b1e2a50237d489b5a1ac57670bf
bigdrivernet <you@example.com>
2023-01-10 14:45:54 +0100

    Added consistent formating if one candidate gets zero votes.

--- Wahl-O-Mat/Program.cs
+++ Wahl-O-Mat/Program.cs
@@ -9,12 +9,12 @@ namespace WahlOMat
         {
             uint uiVotes1, uiVotes2, uiVotesTotal = 0;
             float fPercent1, fPercent2 = 0;
-            char cTopLeft = (char)9556;
-            char cHorizontal = (char)9552;
-            char cTopRight = (char)9559;
-            char cBottomLeft = (char)9562;
-            char cBottomRight = (char)9565;
-            char cVertical = (char)9553;
+            char cTopLeft = (char)0x2554;
+            char cHorizontal = (char)0x2550;
+            char cTopRight = (char)0x2557;
+            char cBottomLeft = (char)0x255A;
+            char cBottomRight = (char)0x255D;
+            char cVertical = (char)0x2551;
 
             Console.WriteLine("--------Wahl-O-Mat--------\n");
 
@@ -31,17 +31,17 @@ namespace WahlOMat
             fPercent2 = ((float)uiVotes2 / (float)uiVotesTotal) * 100; //Calc percentage for Candidate 2
 
             Console.Write(cTopLeft);
-            for (int i = 0; i <= 33; i++) {
+            for (int i = 0; i <= 34; i++) {
                 Console.Write(cHorizontal);
             }
             Console.WriteLine(cTopRight);
 
-            Console.WriteLine($"{cVertical} Das amtliche Endergebnis lautet: {cVertical}");
-            Console.WriteLine($"{cVertical}  Kandidat 1 (in Prozent): {fPercent1:##0.00}  {cVertical}"); //Print percentage for candidate 1 with two decimal places
-            Console.WriteLine($"{cVertical}  Kandidat 2 (in Prozent): {fPercent2:##0.00}  {cVertical}"); //Print percentage for candidate 2 with two decimal places
+            Console.WriteLine($"{cVertical} Das amtliche Endergebnis lautet:  {cVertical}");
+            Console.WriteLine($"{cVertical}  Kandidat 1 (in Prozent): {fPercent1,6:##0.00}  {cVertical}"); //Print percentage for candidate 1 with two decimal places and left padding
+            Console.WriteLine($"{cVertical}  Kandidat 2 (in Prozent): {fPercent2,6:##0.00}  {cVertical}"); //Print percentage for candidate 2 with two decimal places and left padding
             
             Console.Write(cBottomLeft);
-            for (int i = 0; i <= 33; i++) {
+            for (int i = 0; i <= 34; i++) {
                 Console.Write(cHorizontal);
             }
             Console.WriteLine(cBottomRight);
