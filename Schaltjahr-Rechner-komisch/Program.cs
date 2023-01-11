0b8de104ec577fffea268c838e0cad1361c000fb
bigdrivernet <you@example.com>
2023-01-11 09:03:41 +0100

    Schaltjahr-Rechner komisch done

---
+++ Schaltjahr-Rechner-komisch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Schaltjahr_Rechner_komisch
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            uint uiYear;
+            bool bLeapYear = false;
+
+            Console.Write("Bitte geben Sie ein Jahr ein: ");
+            uiYear = Convert.ToUInt32(Console.ReadLine());
+
+            if (uiYear % 4 == 0) {
+                if (uiYear % 100 == 0) {
+                    if (uiYear % 400 == 0) {
+                        bLeapYear = true;
+                    }
+                    else {
+                        bLeapYear = false;
+                    }
+                }
+                else {
+                    bLeapYear = true;
+                }
+            }
+            else {
+                bLeapYear = false;
+            }
+
+            Console.WriteLine($"{uiYear} ist {string.Format("{0:ein;0;kein}", bLeapYear.GetHashCode())} Schaltjahr.");
+        }
+    }
+}
