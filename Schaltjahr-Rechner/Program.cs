f659a8ea37132e5a08bcde9e99bf8a11630de513
bigdrivernet <you@example.com>
2023-01-11 08:35:49 +0100

    Schaltjahr-Rechner done

---
+++ Schaltjahr-Rechner/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Schaltjahr_Rechner
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            uint uiYear;
+            bool bLeapYear = false;
+
+            Console.Write("Bitte geben Sie ein Jahr ein: ");
+            uiYear = Convert.ToUInt32(Console.ReadLine());
+
+            if (((uiYear % 4 == 0) && !(uiYear % 100 == 0))  || uiYear % 400 == 0) {
+                bLeapYear = true;
+            }
+
+            Console.WriteLine($"{uiYear} ist {string.Format("{0:ein;0;kein}", bLeapYear.GetHashCode())} Schaltjahr.");
+        }
+    }
+}
