21ca46eb3b856cfe9fc4d8750c682f330e790e86
bigdrivernet <you@example.com>
2023-01-17 14:34:38 +0100

    Operatoren und Schleife hinzugefügt

--- PraktikantenRechner/Program.cs
+++ PraktikantenRechner/Program.cs
@@ -10,29 +10,40 @@ namespace ParktikantenCode
             double zahl2 = 0.0;
             double ergebnis = 0.0;                              //Variablen für Zahlen
             char rechenzeichen;                                 //Variable für Rechenzeichen
+            char cWiederholen;
 
-            Console.Write("Geben Sie die erste Zahl ein: ");    //Eingabeaufforderung ausgeben
-            zahl1 = Convert.ToDouble(Console.ReadLine());
+            while (true){
+                Console.Write("Geben Sie die erste Zahl ein: ");    //Eingabeaufforderung ausgeben
+                zahl1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Geben Sie das Rechenzeichen ein: "); //Eingabeaufforderung ausgeben
-            rechenzeichen = Convert.ToChar(Console.ReadLine());
+                Console.Write("Geben Sie das Rechenzeichen ein: "); //Eingabeaufforderung ausgeben
+                rechenzeichen = Convert.ToChar(Console.ReadLine());
 
-            Console.Write("Geben Sie die zweite Zahl ein: ");   //Eingabeaufforderung ausgeben
-            zahl2 = Convert.ToDouble(Console.ReadLine());       //Eingabe einlesen
+                Console.Write("Geben Sie die zweite Zahl ein: ");   //Eingabeaufforderung ausgeben
+                zahl2 = Convert.ToDouble(Console.ReadLine());       //Eingabe einlesen
 
-            if (rechenzeichen == '+')                             //je nach Operator berechnen
-                ergebnis = zahl1 + zahl2;
-            if (rechenzeichen == '-')
-                ergebnis = zahl1 - zahl2;
-            if (rechenzeichen == '*')
-                ergebnis = zahl1 * zahl2;
-            if (rechenzeichen == '/')
-                ergebnis = zahl1 / zahl2;
+                if (rechenzeichen == '+')                             //je nach Operator berechnen
+                    ergebnis = zahl1 + zahl2;
+                if (rechenzeichen == '-')
+                    ergebnis = zahl1 - zahl2;
+                if (rechenzeichen == '*')
+                    ergebnis = zahl1 * zahl2;
+                if (rechenzeichen == '/')
+                    ergebnis = zahl1 / zahl2;
+                if (rechenzeichen == '%')
+                    ergebnis = zahl1 % zahl2;
+                if (rechenzeichen == '^')
+                    ergebnis = Math.Pow(zahl1, zahl2);
 
-            //Aufgabe noch mal komplett ausgeben
-            Console.WriteLine($"{zahl1} {rechenzeichen} {zahl2} = {ergebnis}");
+                //Aufgabe noch mal komplett ausgeben
+                Console.WriteLine($"{zahl1} {rechenzeichen} {zahl2} = {ergebnis}");
 
-            Console.ReadKey();
+                Console.Write("Wollen Sie weiterechnen? (Y/N) ");
+                cWiederholen = Convert.ToChar(Console.ReadLine());
+
+                if ((cWiederholen != 'y') && (cWiederholen != 'Y'))
+                    break;
+            }
         }
     }
 }
