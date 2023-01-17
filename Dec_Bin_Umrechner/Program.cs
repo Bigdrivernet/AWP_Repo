83b7dd22ee776349f095164903b36ab6320fe429
bigdrivernet <you@example.com>
2023-01-17 15:15:00 +0100

    Dec to Bin Converter done

---
+++ Dec_Bin_Umrechner/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PraktikantenRechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int iDezimal;
+
+            Console.Write("Bitte geben Sie eine Zahl ein: ");
+            iDezimal = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Convert.ToString(iDezimal, 2));
+        }
+    }
+}
